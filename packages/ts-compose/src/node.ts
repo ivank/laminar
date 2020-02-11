3d6c44faa3accebbfbe5ebc61574dca550094b4e
ivank <ikerin@gmail.com>
2020-02-11 20:38:40 +0200

    Add const and namespace nodes

--- packages/ts-compose/src/node.ts
+++ packages/ts-compose/src/node.ts
@@ -1,4 +1,6 @@
 import * as ts from 'typescript';
+import { withJSDoc } from './docs';
+import { Export } from './type';
 
 export const Identifier = (name: string | ts.Identifier): ts.Identifier =>
   typeof name === 'string' ? ts.createIdentifier(name) : name;
@@ -33,4 +35,60 @@ export const Import = ({
         : undefined,
     ),
     ts.createStringLiteral(module),
+  );
+
+export const NamespaceBlock = ({
+  name,
+  block,
+  isExport,
+  isDefault,
+  jsDoc,
+}: {
+  name: string;
+  block: ts.Statement[];
+  isExport?: boolean;
+  isDefault?: boolean;
+  jsDoc?: string;
+}): ts.ModuleDeclaration =>
+  withJSDoc(
+    ts.createModuleDeclaration(
+      [],
+      Export(isExport, isDefault),
+      ts.createIdentifier(name),
+      ts.createModuleBlock(block),
+      ts.NodeFlags.Namespace,
+    ),
+    jsDoc,
+  );
+
+export const Const = ({
+  name,
+  type,
+  value,
+  isExport,
+  isDefault,
+  jsDoc,
+}: {
+  name: string | ts.Identifier;
+  type?: ts.TypeNode;
+  value?: string | number | boolean;
+  isExport?: boolean;
+  isDefault?: boolean;
+  jsDoc?: string;
+}): ts.VariableStatement =>
+  withJSDoc(
+    ts.createVariableStatement(
+      Export(isExport, isDefault),
+      ts.createVariableDeclarationList(
+        [
+          ts.createVariableDeclaration(
+            name,
+            type,
+            value === undefined ? undefined : ts.createLiteral(value),
+          ),
+        ],
+        ts.NodeFlags.Const,
+      ),
+    ),
+    jsDoc,
   );
--- packages/ts-compose/test/Node.spec.ts
+++ packages/ts-compose/test/Node.spec.ts
@@ -16,9 +16,34 @@ describe('TS Compose', () => {
       'import Axios, { AxiosInstance as one, AxiosRequestConfig as two } from "axios";',
       Node.Import({
         defaultAs: 'Axios',
-        named: [{ name: 'AxiosInstance', as: 'one' }, { name: 'AxiosRequestConfig', as: 'two' }],
+        named: [
+          { name: 'AxiosInstance', as: 'one' },
+          { name: 'AxiosRequestConfig', as: 'two' },
+        ],
         module: 'axios',
       }),
+    ],
+    ['namespace a { }', Node.NamespaceBlock({ name: 'a', block: [] })],
+    [
+      'export default namespace a { }',
+      Node.NamespaceBlock({ isExport: true, isDefault: true, name: 'a', block: [] }),
+    ],
+    [
+      'export namespace test {\n    const a = 10;\n    const b = true;\n}',
+      Node.NamespaceBlock({
+        name: 'test',
+        isExport: true,
+        block: [Node.Const({ name: 'a', value: 10 }), Node.Const({ name: 'b', value: true })],
+      }),
+    ],
+    ['const a;', Node.Const({ name: 'a' })],
+    ['const s = "10";', Node.Const({ name: 's', value: '10' })],
+    ['const b = true;', Node.Const({ name: 'b', value: true })],
+    ['const i = 123;', Node.Const({ name: 'i', value: 123 })],
+    ['export const b = false;', Node.Const({ name: 'b', isExport: true, value: false })],
+    [
+      'export default const b = true;',
+      Node.Const({ name: 'b', isExport: true, isDefault: true, value: true }),
     ],
   ])('Test print of %s', async (typescript, result) => {
     expect(printNode(result)).toEqual(typescript);
