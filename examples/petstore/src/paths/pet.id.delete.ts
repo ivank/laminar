e5732dda8ea71f76666871f095020172bd33d370
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2021-06-07 10:44:39 +0300

    corrected typos in comment (#65)

--- examples/petstore/src/paths/pet.id.delete.ts
+++ examples/petstore/src/paths/pet.id.delete.ts
@@ -3,12 +3,12 @@ import { jsonNotFound, jsonNoContent } from '@ovotech/laminar';
 import { PetsDbContext } from '../middleware';
 
 /**
- * Request and response are both validated runtime and checked at compoile time
+ * Request and response are both validated runtime and checked at compile time
  * That's why we need to use `jsonNoContent` and `jsonNotFound` to set the correct Content-Type header and status.
  *
- * Laminar provides helpers for common status codes and content types, but its easy to roll your own
+ * Laminar provides helpers for common status codes and content types, but it's easy to roll your own
  *
- * Anithing that's deviating from the OpenAPI schema - the shape of the json response, status or content-type
+ * Anything that's deviating from the OpenAPI schema - the shape of the json response, status or content-type
  * would be flagged by typescript as an error
  */
 export const pathPetsIdDelete: PathPetsIdDelete<PetsDbContext> = async ({ petsDb, path: { id } }) => {
