ecf393ce223b2471c7a934b4cec315244c175d3b
Ivan Kerin <ikerin@gmail.com>
2022-03-31 11:39:55 +0300

    Nullable wrapped unions for avsc (#116)

--- packages/laminar-cli/src/commands/avro/types/union.ts
+++ packages/laminar-cli/src/commands/avro/types/union.ts
@@ -3,7 +3,8 @@ import { Convert } from '../types';
 import { Type, mapWithContext, document } from '@ovotech/ts-compose';
 import { convertType } from '../convert';
 
-export const isUnion = (type: Schema): type is schema.NamedType[] => typeof type === 'object' && Array.isArray(type);
+export const isUnion = (type: Schema): type is (schema.NamedType | schema.PrimitiveType)[] =>
+  typeof type === 'object' && Array.isArray(type);
 
 export const convertUnionType: Convert<schema.DefinedType[]> = (context, schema) => {
   const map = mapWithContext(context, schema, convertType);
